e9026b9bb52b4bbdad0af8bb64e7b80cc7a111c0
Cornelius <sandmal.cornelius@gmail.com>
2022-02-24 16:27:10 +0100

    Added user controller for creating a user

---
+++ controllers/user.controllers.js
@@ -0,0 +1,42 @@
+import userModel from '../models/user.js';
+import bcrypt from 'bcryptjs';
+
+//
+// @USER
+//
+
+// Super Admins can invite users
+export const createUser = async (req, res) => {
+  const { username, role, email, password, programmeCode, year } = req.body;
+  //validate fields
+  if (!username || !role || !password) {
+    return res.status(400).json({ error: 'username, role, email or password is required' });
+  }
+
+  const userExists = await userModel.exists({ username });
+  if (userExists) return res.status(400).json({ error: 'User already exists' });
+
+  if (email) {
+    const emailExists = await userModel.exists({ email });
+    if (emailExists) return res.status(400).json({ error: 'User with email already exists' });
+  }
+
+  const passwordHash = bcrypt.hashSync(password, 10);
+
+  const user = new userModel({
+    username,
+    role,
+    email,
+    password: passwordHash,
+    programmeCode,
+    year,
+  });
+
+  console.log(user);
+  try {
+    await user.save();
+    res.status(201).json({ message: 'User created successfully', username, role, email, programmeCode, year });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error when creating user', error });
+  }
+};
