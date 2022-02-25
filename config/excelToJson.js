c3ad88f668f130676bc40377b1d75eb0ff2a4c74
Cornelius <sandmal.cornelius@gmail.com>
2022-02-25 18:28:34 +0100

    Added excelToJson code

---
+++ config/excelToJson.js
@@ -0,0 +1,44 @@
+import excelToJson from 'convert-excel-to-json';
+import fs from 'fs';
+
+export const readDataFromExcel = (file) => {
+  const sourceFile = excelToJson({
+    source: fs.readFileSync(file),
+    range: 'B5',
+  });
+
+  const numberOfSheets = sourceFile['Overview'][0].B[0];
+
+  const returnAllSheets = (sheets) => {
+    let array = [];
+    for (let i = 0; i < sheets; i++) {
+      array.push(`${i + 1} Quiz`);
+    }
+    return array;
+  };
+
+  const allQuizSheets = excelToJson({
+    source: fs.readFileSync(file),
+    header: { rows: 3 },
+    sheets: [
+      {
+        name: 'Final Scores',
+        header: { rows: 3 },
+        columnToKey: {
+          A: 'Rank',
+          B: 'Player',
+          C: 'scorePoints',
+          D: 'correctAnswers',
+          E: 'inCorrectAnswers',
+        },
+      },
+      {
+        name: 'Overview',
+        header: { rows: 1 },
+        range: 'A2:B5',
+      },
+    ],
+  });
+
+  return allQuizSheets;
+};
