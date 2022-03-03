321403390f10f1d64a6984217a11151f8ba70678
Sandmal <sandmal.cornelius@gmail.com>
2022-03-03 17:54:58 +0100

    edit to semester schema

--- models/semester.js
+++ models/semester.js
@@ -3,22 +3,12 @@ const { Schema, model } = mongoose;
 
 const semesterSchema = new Schema(
   {
-    year: {
-      type: 'string',
-      trim: true,
-      lowercase: true,
-      required: true,
-    },
     periodNumber: {
       type: 'number',
       trim: true,
       required: true,
       min: 1,
       max: 6,
-    },
-    type: {
-      enum: ['fall', 'spring'],
-      required: true,
     },
     courseGroups: {
       type: 'array',
