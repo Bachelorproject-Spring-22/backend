cf301bb2cb563ab3c0887dc86f3bb81a6f116332
Sandmal <sandmal.cornelius@gmail.com>
2022-03-08 17:39:06 +0100

    Added studyPeriod and StudyProgramme to JWT token

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -31,7 +31,7 @@ export const login = async (req, res) => {
       jwtToken,
     });
   } catch (error) {
-    res.status(500).json({ error: 'There was a server-side error with login. Please try again.', error });
+    res.status(500).json({ error: 'There was a server-side error with login. Please try again.' });
   }
 };
 
@@ -124,6 +124,23 @@ function setTokenCookie(res, token) {
   res.cookie('refreshToken', token, cookieOptions);
 }
 
+function calculateSemester(startYear) {
+  const getCurrentDate = Date.now();
+  const date = new Date(getCurrentDate);
+  const onlyYear = date.getFullYear();
+  const onlyMonth = date.getMonth() + 1;
+  const currentYear = onlyYear - startYear;
+  let term, studyPeriod;
+  onlyMonth < 7 ? (term = 'spring') : (term = 'fall');
+  term == 'fall' ? currentYear++ : currentYear;
+
+  if (currentYear == 1) return term == 'fall' ? (studyPeriod = 1) : (studyPeriod = 2);
+  if (currentYear == 2) return term == 'fall' ? (studyPeriod = 3) : (studyPeriod = 4);
+  if (currentYear == 3) return term == 'fall' ? (studyPeriod = 5) : (studyPeriod = 6);
+  if (currentYear == 4) return term == 'fall' ? (studyPeriod = 7) : (studyPeriod = 8);
+  if (currentYear == 5) return term == 'fall' ? (studyPeriod = 9) : (studyPeriod = 10);
+}
+
 async function getRefreshToken(token) {
   const refreshToken = await refreshTokenModel.findOne({ token }).populate('user');
   if (!refreshToken || !refreshToken.isActive) throw 'Invalid token';
@@ -132,9 +149,14 @@ async function getRefreshToken(token) {
 
 function generateJwtToken(user) {
   const exp = process.env.JWT_TOKEN_EXP_IN_MINUTES;
-  return jwt.sign({ _id: user.id, role: user.role }, process.env.TOKEN_SECRET, {
-    expiresIn: `${exp}`,
-  });
+  const studyPeriod = calculateSemester(user.year);
+  return jwt.sign(
+    { _id: user.id, role: user.role, studyProgrammeCode: user.programmeCode, studyPeriod },
+    process.env.TOKEN_SECRET,
+    {
+      expiresIn: `${exp}`,
+    },
+  );
 }
 
 function generateRefreshToken(user, ipAddress) {
