a550316f66ae2fb501c382a73114117c1713d61a
Sandmal <sandmal.cornelius@gmail.com>
2022-03-14 15:40:48 +0100

    Changed studyPeriod to periodNumber in jwt token

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -151,7 +151,13 @@ function generateJwtToken(user) {
   const exp = process.env.JWT_TOKEN_EXP_IN_MINUTES;
   const studyPeriod = calculateSemester(user.year);
   return jwt.sign(
-    { _id: user.id, role: user.role, username: user.username, studyProgrammeCode: user.programmeCode, studyPeriod },
+    {
+      _id: user.id,
+      role: user.role,
+      username: user.username,
+      studyProgrammeCode: user.programmeCode,
+      periodNumber: studyPeriod,
+    },
     process.env.TOKEN_SECRET,
     {
       expiresIn: `${exp}`,
