c76021eda8e3b18518d5a5300d9a6a06af9dd9fd
Sandmal <sandmal.cornelius@gmail.com>
2022-03-17 19:12:26 +0100

    Add selectQuizSnapshot route

--- routes/leaderboard.routes.js
+++ routes/leaderboard.routes.js
@@ -3,6 +3,7 @@ import express from 'express';
 import {
   semesterLeaderboardAndUserCourses,
   courseSpecificLeaderboard,
+  selectQuizSnapshot,
 } from '../controllers/leaderboard.controllers.js';
 import asyncMiddleware from '../middleware/async.middleware.js';
 
@@ -11,5 +12,7 @@ const router = express.Router();
 router.get('/', asyncMiddleware(semesterLeaderboardAndUserCourses));
 
 router.get('/:courseId', asyncMiddleware(courseSpecificLeaderboard));
+
+router.post('/:courseId', asyncMiddleware(selectQuizSnapshot));
 
 export default router;
