c9033db2553bfa5fc90f009e885663239e16593b
Sandmal <sandmal.cornelius@gmail.com>
2022-03-18 13:54:15 +0100

    Fix structure of payload from leaderboard /:courseId

--- controllers/leaderboard.controllers.js
+++ controllers/leaderboard.controllers.js
@@ -284,7 +284,7 @@ export const courseSpecificLeaderboard = async (req, res) => {
   // Add anonymous usernames except top 5 or current user
 
   const studyProgrammeData = getUserData.map(({ player, rank }) =>
-    rank <= 5 || player._id === username ? { player, rank } : { ...player, _id: 'Anonymous', rank },
+    rank <= 5 || player._id === username ? { player, rank } : { player: { ...player, _id: 'Anonymous' }, rank },
   );
 
   res.status(201).json({
