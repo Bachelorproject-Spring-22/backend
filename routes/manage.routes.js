25b97c5274c22482a43b8a2c88e883a30c89554b
Sandmal <sandmal.cornelius@gmail.com>
2022-03-28 15:45:46 +0200

    Delete course from /manage/

--- routes/manage.routes.js
+++ routes/manage.routes.js
@@ -1,10 +1,8 @@
 import express from 'express';
-import { updateUserWithCourses, updateUserWithStudyplan } from '../controllers/employee.controllers.js';
+import { updateUserWithStudyplan } from '../controllers/employee.controllers.js';
 import asyncMiddleware from '../middleware/async.middleware.js';
 
 const router = express.Router();
-
-router.post('/course', asyncMiddleware(updateUserWithCourses));
 router.post('/programme', asyncMiddleware(updateUserWithStudyplan));
 
 export default router;
