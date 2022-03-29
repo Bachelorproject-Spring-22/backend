9f70e367a45215a5157b1e180d94504316003a9f
Sandmal <sandmal.cornelius@gmail.com>
2022-03-29 12:34:28 +0200

    Add previous upload routes and new GET /programme route

--- routes/manage.routes.js
+++ routes/manage.routes.js
@@ -1,8 +1,32 @@
 import express from 'express';
-import { updateUserWithStudyplan, deleteQuizFromCourse } from '../controllers/employee.controllers.js';
+import {
+  updateUserWithStudyplan,
+  deleteQuizFromCourse,
+  quizUpload,
+  getUserSpecificCourseAndStudyprogrammeCode,
+  getAllStudyPlans,
+} from '../controllers/employee.controllers.js';
 import asyncMiddleware from '../middleware/async.middleware.js';
 
+import multer from 'multer';
+
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './tmp');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: fileStorageEngine });
 const router = express.Router();
+
+router.post('/', upload.single('file'), asyncMiddleware(quizUpload));
+
+router.get('/', asyncMiddleware(getUserSpecificCourseAndStudyprogrammeCode));
+
+router.get('/programme', asyncMiddleware(getAllStudyPlans));
 router.post('/programme', asyncMiddleware(updateUserWithStudyplan));
 
 router.delete('/courses/:courseId/:quizId', asyncMiddleware(deleteQuizFromCourse));
