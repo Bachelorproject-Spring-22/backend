31e8f2c4b46cfe63320af32936b553fc0d9e372f
Sandmal <sandmal.cornelius@gmail.com>
2022-03-29 17:49:48 +0200

    Add documentation to superAdmin route

--- routes/superAdmin.routes.js
+++ routes/superAdmin.routes.js
@@ -11,6 +11,15 @@ import { updateUserWithStudyplan } from '../controllers/employee.controllers.js'
 
 const router = express.Router();
 
+/*
+  ROUTE superAdmin '/superAdmin'
+  POST /user                                  | Input: Username, role, email, password, programmeCode, year
+  POST /course                                | Input: code, name, credits, year, semester, activities
+  POST /programme                             | Input: ProgrammeCode, year, name, startTerm, semesters
+  PATCH /programme/:studyProgrammeCode        | Input: array[username(s)]
+  PATCH /programme/:courseId                  | Input: periodNumber, courseId
+*/
+
 router.post('/user', asyncMiddleware(createUser));
 
 router.post('/course', asyncMiddleware(createCourse));
