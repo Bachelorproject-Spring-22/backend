d1d220056058dcbec20c334d97f27404f2fa523a
Sandmal <sandmal.cornelius@gmail.com>
2022-03-29 12:12:12 +0200

    Add DELETE route

--- routes/manage.routes.js
+++ routes/manage.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { updateUserWithStudyplan } from '../controllers/employee.controllers.js';
+import { updateUserWithStudyplan, deleteQuizFromCourse } from '../controllers/employee.controllers.js';
 import asyncMiddleware from '../middleware/async.middleware.js';
 
 const router = express.Router();
 router.post('/programme', asyncMiddleware(updateUserWithStudyplan));
+
+router.delete('/courses/:courseId/:quizId', asyncMiddleware(deleteQuizFromCourse));
 
 export default router;
