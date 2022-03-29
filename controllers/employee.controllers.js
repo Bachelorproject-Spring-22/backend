d47655539076d8db836abee6cb2bf080a64b8a1d
Sandmal <sandmal.cornelius@gmail.com>
2022-03-29 12:34:08 +0200

    Add previous upload controllers to manage and add GET studyplans

--- controllers/employee.controllers.js
+++ controllers/employee.controllers.js
@@ -3,11 +3,118 @@ import studyProgrammeModel from '../models/studyProgramme.js';
 import courseModel from '../models/course.js';
 import kahootModel from '../models/kahoot.js';
 import generateJwtToken from '../utils/generateTokens.js';
-import mongoose from 'mongoose';
 
+import calculateSemester from '../utils/calculateSemester.js';
+import { readDataFromExcel } from '../config/excelToJson.js';
 import { createBadRequest, createNotFound } from '../utils/errors.js';
 
-const ObjectId = mongoose.Types.ObjectId;
+export const quizUpload = async (req, res, next) => {
+  const filePath = req.file;
+  const courseId = req.body.courseId;
+
+  if (!courseId) return next(createNotFound('Course id not found'));
+  if (!filePath) return next(createNotFound('filePath'));
+
+  const name = 'kahoot';
+  const variant = 'quiz';
+
+  if (!filePath) return next(createBadRequest('Please upload a file'));
+  const dataFromExcel = readDataFromExcel(req.file.path);
+  if (!dataFromExcel) return next(createBadRequest('Upload a valid file'));
+
+  const finalScores = dataFromExcel['Final Scores'].map((user) => user);
+  const activityIds = [];
+
+  const course = await courseModel.findOne({ courseId });
+  if (!course) return next(createNotFound('Course not found'));
+
+  course.activities.forEach((kahoot) => {
+    if (kahoot.name === name && kahoot.variant === variant) {
+      const [kahoots] = kahoot.sources;
+      activityIds.push(kahoots);
+    }
+  });
+
+  const titleFromExcel = dataFromExcel['Overview'][0].A;
+  var sanitized = titleFromExcel.replace(/[^\w\s]/gi, '');
+  const title = sanitized.replace(/\s+/g, '_');
+  const utc = dataFromExcel['Overview'][1].B;
+
+  // https://stackoverflow.com/questions/38735927/add-offset-to-utc-date-in-javascript
+  // Add one hour to playedOn because of timezone difference
+  const playedOn = new Date(new Date(utc) * 1 + 60 * 60 * 1000);
+  const kahoot = new kahootModel({
+    title,
+    playedOn,
+    hostedBy: dataFromExcel['Overview'][2].B,
+    numberOfPlayers: dataFromExcel['Overview'][3].B,
+    course: {
+      code: course.code,
+      name: course.name,
+      courseId,
+    },
+    finalScores,
+  });
+
+  const checkIfQuizExists = await kahootModel.findOne({ quizId: kahoot.quizId }, { quizId: 1, _id: 0 }).lean();
+  if (checkIfQuizExists) return next(createBadRequest(`Quiz ${title} is already uploaded to this course`));
+
+  await kahoot.save().then(async ({ _id }) => {
+    await courseModel.updateOne(
+      { courseId },
+      { $push: { 'activities.$[activities].sources': _id } },
+      { arrayFilters: [{ 'activities.name': name }] },
+    );
+  });
+  res.status(201).json({ message: 'Quiz uploaded successfully' });
+};
+
+export const getUserSpecificCourseAndStudyprogrammeCode = async (req, res, next) => {
+  const user = req.user;
+  if (!user) next(createNotFound('User not found'));
+  if (!user.courses) next(createNotFound('Course(s) not found'));
+  const studyProgrammeCode = user.studyProgrammes;
+
+  let codes = [];
+  let number = [];
+
+  studyProgrammeCode.forEach((code) => {
+    let filterOutstring = code.match(/\d+/g);
+    let createYear = `20${filterOutstring}`;
+    let periodNumber = calculateSemester(createYear);
+    codes.push({ studyProgrammeCode: code, periodNumber });
+    number.push(periodNumber);
+  });
+
+  const studyProgrammeData = await studyProgrammeModel.aggregate([
+    { $match: { studyProgrammeCode: { $in: studyProgrammeCode } } },
+    { $unwind: '$studyPeriods' },
+    { $match: { 'studyPeriods.periodNumber': { $in: number } } },
+    { $unwind: '$studyPeriods.courses' },
+    {
+      $lookup: {
+        from: 'courses',
+        localField: 'studyPeriods.courses',
+        foreignField: '_id',
+        as: 'coursesInPeriod',
+      },
+    },
+    { $unwind: '$coursesInPeriod' },
+  ]);
+  const array = [];
+  codes.forEach((code) =>
+    studyProgrammeData.forEach(
+      (data) =>
+        code.studyProgrammeCode === data.studyProgrammeCode &&
+        code.periodNumber === data.studyPeriods.periodNumber &&
+        array.push(data.coursesInPeriod),
+    ),
+  );
+
+  const courseIds = [...new Set(array.map((item) => item.courseId))];
+
+  res.status(201).json({ message: 'CourseId(s) found', courseIds });
+};
 
 export const updateUserWithStudyplan = async (req, res, next) => {
   const { studyProgrammeCode } = req.body;
@@ -89,5 +196,18 @@ export const deleteQuizFromCourse = async (req, res, next) => {
 
   res.status(201).json({
     message: 'Quizz deleted successfully',
+  });
+};
+
+export const getAllStudyPlans = async (req, res, next) => {
+  const user = req.user;
+  if (!user) return next(createNotFound('User not found'));
+
+  const studyProgrammeCodes = await studyProgrammeModel.find({}, { studyProgrammeCode: 1, _id: 0 });
+  if (!studyProgrammeCodes) return next(createNotFound('Studyprogrammes not found'));
+
+  res.status(201).json({
+    message: 'All studyProgrammeCodes',
+    studyProgrammeCodes,
   });
 };
