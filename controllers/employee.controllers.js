fd1314e1301e34a05043b83060edf9c63b91d90e
Sandmal <sandmal.cornelius@gmail.com>
2022-03-29 12:11:40 +0200

    Delete quiz from course and kahoot

--- controllers/employee.controllers.js
+++ controllers/employee.controllers.js
@@ -1,14 +1,19 @@
 import userModel from '../models/user.js';
 import studyProgrammeModel from '../models/studyProgramme.js';
+import courseModel from '../models/course.js';
+import kahootModel from '../models/kahoot.js';
 import generateJwtToken from '../utils/generateTokens.js';
+import mongoose from 'mongoose';
 
 import { createBadRequest, createNotFound } from '../utils/errors.js';
+
+const ObjectId = mongoose.Types.ObjectId;
 
 export const updateUserWithStudyplan = async (req, res, next) => {
   const { studyProgrammeCode } = req.body;
   const user = req.user;
   const { username } = user;
-  if (!studyProgrammeCode) return next(createNotFound('Course not found'));
+  if (!studyProgrammeCode) return next(createNotFound('StudyProgrammeCode not found'));
 
   const checkUser = await userModel.findOne({ username }, { studyProgrammes: 1 }).sort({ _id: 1 }).lean();
   if (!checkUser) return next(createBadRequest('User(s) does not exist'));
@@ -48,5 +53,41 @@ export const updateUserWithStudyplan = async (req, res, next) => {
   res.status(201).json({
     message: `Courses added successfully [${studyProgrammes}]`,
     jwtToken,
+  });
+};
+
+export const deleteQuizFromCourse = async (req, res, next) => {
+  const { courseId, quizId } = req.params;
+  if (!courseId) return next(createNotFound('Course not found'));
+  if (!quizId) return next(createNotFound('Quiz not found'));
+
+  const checkIfCourseExists = await courseModel.findOne({ courseId });
+  if (!checkIfCourseExists) return next(createNotFound('Course not found'));
+
+  const courseQuizzes = checkIfCourseExists.activities[0].sources;
+  const checkIfQuizzesExists = await kahootModel.find({ quizId });
+  if (checkIfQuizzesExists.length === 0) return next(createNotFound('Quiz not found'));
+
+  const quizzesToDelete = [];
+
+  checkIfQuizzesExists.forEach((quiz) => {
+    courseQuizzes.forEach((item) => {
+      if (quiz._id.toString() === item.toString()) {
+        quizzesToDelete.push(item);
+      }
+    });
+  });
+
+  // Delete kahoot _id from course
+  await courseModel.updateOne(
+    { courseId },
+    { $pull: { 'activities.$[activities].sources': { $in: quizzesToDelete } } },
+    { arrayFilters: [{ 'activities.name': 'kahoot' }] },
+  );
+  // Delete kahoot by quizId
+  await kahootModel.findOneAndDelete({ quizId });
+
+  res.status(201).json({
+    message: 'Quizz deleted successfully',
   });
 };
