bfc3804b76e70ebad5e1f27e898621a46a5fd090
Sandmal <sandmal.cornelius@gmail.com>
2022-03-30 15:52:11 +0200

    Remove unused code

--- utils/generateTokens.js
+++ utils/generateTokens.js
@@ -1,16 +1,14 @@
 import jwt from 'jsonwebtoken';
 import calculateSemester from './calculateSemester.js';
-export default function generateJwtToken(user, studyProgrammeCode, year, programmes) {
-  const programmeCode = user.programmeCode ? user.programmeCode : studyProgrammeCode;
-  const programmeYear = user.year ? user.year : year;
+export default function generateJwtToken(user, programmes) {
   const studyProgrammes = user.studyProgrammes.length !== 0 ? user.studyProgrammes : programmes;
-  const studyPeriod = calculateSemester(programmeYear, user.role, programmeCode);
+  const studyPeriod = calculateSemester(user.year, user.role, user.programmeCode);
   return jwt.sign(
     {
       _id: user.id,
       role: user.role,
       username: user.username,
-      studyProgrammeCode: programmeCode,
+      studyProgrammeCode: user.programmeCode,
       periodNumber: studyPeriod,
       studyProgrammes: user.role !== 'student' ? studyProgrammes : null,
     },
